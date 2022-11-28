467dcdf89a85aa34a758eb7eb824a8f94cbc052f
EloArtzl <ac.eloise@gmail.com>
2022-11-28 16:56:20 +0100

    ajout de l'icone, creation des styles de polices, modification de la page principale, reimplementation des polices

--- app/src/main/java/com/example/foodigo1/MainActivity.java
+++ app/src/main/java/com/example/foodigo1/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.foodigo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        System.out.println("le jeu commence");
     }
 }
